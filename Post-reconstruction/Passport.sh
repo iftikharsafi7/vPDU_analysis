df8a5df3cd0f0a28adc7b4104d89c0eaf6e5181d
Iftikhar Ahmad <50864942+iftikharsafi7@users.noreply.github.com>
2025-02-10 16:52:17 +0100

    Update Passport.sh

--- Post-reconstruction/Passport.sh
+++ Post-reconstruction/Passport.sh
@@ -1,3 +1,4 @@
+#Running passport on multiple reconstructed files
 # Define the run numbers in an array
 file_numbers=(419 448 449 450 451 452 453 454)
 
@@ -5,7 +6,7 @@ file_numbers=(419 448 449 450 451 452 453 454)
 for number in "${file_numbers[@]}"
 do
     # Define the input file based on the run number
-    input_file="/Users/astrocent/vPDU_final/reconstructed_root_files_rms_3/run${number}.root"
+    input_file="/directory_of_the_root_file/run${number}.root"
 
     # Automatically pass the run number as the QR code or name
     qr_code="run${number}"
